bfaf1cd49adb7497b3c401d2732d397e0485d077
Matthew Mckenzie <mmckenzie@vostronet.com>
2023-11-22 15:14:41 +1000

    5.3.2

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+  testMatch: ["**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)"],
   collectCoverage: true,
   collectCoverageFrom: [
     "**/*.{ts,js}",
